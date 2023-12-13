0fc05f607873b7b78028530028eef5d180cf39aa
Tanki <mjrcoetzee+1@outlook.com>
2023-12-13 11:52:44 +0200

    Add project files.

---
+++ Visualiser.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using BepInEx;
+using Invector.vMelee;
+
+namespace HitboxVisualiser
+{
+    internal class Visualiser: MonoBehaviour
+    {
+        bool F8Pressed = false;
+        public static bool active = false;
+
+        internal void Update()
+        {
+            foreach (vHitBox hitbox in GameObject.FindObjectsOfType<vHitBox>())
+            {
+                if (!hitbox.gameObject.GetComponent<VisualiserComponent>())
+                {
+                    Plugin.Log.LogInfo($"Adding VisualiserComponent to vHitBox {hitbox.name}");
+                    hitbox.gameObject.AddComponent<VisualiserComponent>();
+                }
+            }
+        }
+    }
+}
