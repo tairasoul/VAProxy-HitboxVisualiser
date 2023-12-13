dea642bad896a4cdf0cb6b7b176557bf6059af80
tairasoul <100938045+tairasoul@users.noreply.github.com>
2023-12-13 11:52:44 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,88 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SettingsAPI;
+using UnityEngine;
+using UnityEngine.UI;
+using Settings = SettingsAPI.Plugin;
+
+namespace HitboxVisualiser
+{
+    [BepInPlugin("vaproxy.hitbox.visualiser", "HitboxVisualiser", "1.0.0")]
+    public class Plugin: BaseUnityPlugin
+    {
+        Harmony harmony = new Harmony("vaproxy.hitbox.visualiser");
+        bool initialized = false;
+        public static ManualLogSource Log;
+        internal void Awake()
+        {
+            Log = Logger;
+            Logger.LogInfo("HitboxVisualiser awake.");
+        }
+
+        internal void Start()
+        {
+            Init();
+        }
+
+        internal void OnDestroy()
+        {
+            Init();
+        }
+
+        internal void Init()
+        {
+            if (!initialized)
+            {
+                initialized = true;
+                GameObject game = new GameObject("HitboxVisualiser");
+                DontDestroyOnLoad(game);
+                game.AddComponent<Visualiser>();
+                Logger.LogInfo("Visualiser initialized.");
+                Option Toggle = new()
+                {
+                    Create = (GameObject Page) =>
+                    {
+                        Log.LogInfo("Creating HitboxVisualiser page init.");
+                        GameObject HitboxToggle = Page.AddObject("HitboxToggle");
+                        HitboxToggle.AddComponent<RectTransform>().anchoredPosition = new Vector2(-449.6534f, 158.6981f);
+                        HitboxToggle.transform.localScale = new Vector3(2.1268f, 2.1268f, 2.1268f);
+                        Toggle toggle = HitboxToggle.AddComponent<Toggle>();
+                        GameObject Label = HitboxToggle.AddObject("Label");
+                        Label.AddComponent<CanvasRenderer>();
+                        Label.AddComponent<RectTransform>().anchoredPosition = new Vector2(52.16f, -1);
+                        Label.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+                        Text text = Label.AddComponent<Text>();
+                        text.alignment = TextAnchor.MiddleLeft;
+                        text.fontSize = 100;
+                        GameObject original = GameObject.Find("MAINMENU/Canvas/Pages/Setting/Resolution/VSync/Background");
+                        text.font = original.transform.parent.gameObject.Find("Label").GetComponent<Text>().font;
+                        text.text = "Visualize Hitboxes";
+                        text.horizontalOverflow = HorizontalWrapMode.Overflow;
+                        text.color = new Color(0.9843f, 0.6902f, 0.2314f);
+                        GameObject background = HitboxToggle.AddObject("Background");
+                        background.AddComponent<RectTransform>().anchoredPosition = new Vector2(10, 0);
+                        background.AddComponent<CanvasRenderer>();
+                        background.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+                        Image image = background.AddComponent<Image>();
+                        image.sprite = original.GetComponent<Image>().sprite;
+                        GameObject checkmark = background.AddObject("Checkmark");
+                        checkmark.AddComponent<RectTransform>();
+                        checkmark.AddComponent<CanvasRenderer>();
+                        Image checkmarkImage = checkmark.AddComponent<Image>();
+                        checkmarkImage.enabled = false;
+                        toggle.image = checkmarkImage;
+                        checkmarkImage.sprite = original.Find("Checkmark").GetComponent<Image>().sprite;
+                        toggle.onValueChanged.AddListener((bool toggled) =>
+                        {
+                            checkmarkImage.enabled = toggled;
+                            Visualiser.active = toggled;
+                        });
+                    }
+                };
+                Option[] Options = [Toggle];
+                Settings.API.RegisterMod("tairasoul.HitboxVisualiser", "HitboxVisualiser", Options, (GameObject obj) => { obj.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f); }) ;
+            }
+        }
+    }
+}
---
+++ VisualiserComponent.cs
@@ -0,0 +1,89 @@
+﻿using Invector.vMelee;
+using UnityEngine;
+
+namespace HitboxVisualiser
+{
+
+    // like half of the shit here was done by chatgpt (update(), drawtrail()) but who gives a shit it works
+    internal class VisualiserComponent: MonoBehaviour
+    {
+        Collider trigger;
+        vHitBox hitBox;
+        private LineRenderer lineRenderer;
+
+        internal void Start()
+        {
+            trigger = gameObject.GetComponent<Collider>();
+            hitBox = gameObject.GetComponent<vHitBox>();
+            if (!trigger)
+            {
+                trigger = gameObject.AddComponent<Collider>();
+            }
+        }
+
+        private void Update()
+        {
+            if (Visualiser.active && lineRenderer == null)
+            {
+                DrawTrail();
+            }
+            else if (!Visualiser.active && lineRenderer != null)
+            {
+                Destroy(lineRenderer);
+            }
+        }
+
+        private void DrawTrail()
+        {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+            lineRenderer.useWorldSpace = false; // Use local space
+            lineRenderer.loop = true; // Close the loop for trail
+            lineRenderer.material = new Material(Shader.Find("Sprites/Default")); // Change the material
+
+            // Adjust width and color of the lines
+            Color color = ((hitBox.triggerType & vHitBoxType.Damage) != (vHitBoxType)0 && (hitBox.triggerType & vHitBoxType.Recoil) == (vHitBoxType)0) ? Color.green : (((hitBox.triggerType & vHitBoxType.Damage) != (vHitBoxType)0 && (hitBox.triggerType & vHitBoxType.Recoil) != (vHitBoxType)0) ? Color.yellow : (((hitBox.triggerType & vHitBoxType.Recoil) != (vHitBoxType)0 && (hitBox.triggerType & vHitBoxType.Damage) == (vHitBoxType)0) ? Color.red : Color.black));
+            color.a = 0.6f;
+            lineRenderer.startWidth = 0.05f;
+            lineRenderer.endWidth = 0.05f;
+            lineRenderer.startColor = color;
+            lineRenderer.endColor = color;
+
+            BoxCollider collider = trigger as BoxCollider;
+
+            Vector3 center = collider.center;
+            Vector3 size = collider.size;
+
+            Vector3[] corners =
+            {
+                center + new Vector3(size.x, size.y, size.z) * 0.5f,
+                center + new Vector3(size.x, size.y, -size.z) * 0.5f,
+                center + new Vector3(-size.x, size.y, -size.z) * 0.5f,
+                center + new Vector3(-size.x, size.y, size.z) * 0.5f,
+                center + new Vector3(size.x, -size.y, size.z) * 0.5f,
+                center + new Vector3(size.x, -size.y, -size.z) * 0.5f,
+                center + new Vector3(-size.x, -size.y, -size.z) * 0.5f,
+                center + new Vector3(-size.x, -size.y, size.z) * 0.5f
+            };
+
+            // Define indices to draw the edges of the box
+            int[] indices =
+            {
+                0, 1, 1, 2, 2, 3, 3, 0, // Top face
+                4, 5, 5, 6, 6, 7, 7, 4, // Bottom face
+                0, 4, 1, 5, 2, 6, 3, 7  // Vertical edges
+            };
+
+            lineRenderer.positionCount = indices.Length;
+
+            for (int i = 0; i < indices.Length; i++)
+            {
+                lineRenderer.SetPosition(i, corners[indices[i]]);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (lineRenderer) Destroy(lineRenderer);
+        }
+    }
+}
